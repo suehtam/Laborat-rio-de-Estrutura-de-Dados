e4a8126da36c7631329ccdd17068e59d83b53073
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-09-05 12:05:27 -0300

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,25 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Troca arr[j+1] e arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Vetor ordenado:");
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
