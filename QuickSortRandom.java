594b5aa825286e823dd4295b1468ce2feef0e7da
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-09-12 11:14:27 -0300

    Update QuickSortRandom.java

--- QuickSortRandom.java
+++ QuickSortRandom.java
@@ -3,15 +3,15 @@ import random
 def quicksort(arr):
     if len(arr) <= 1:
         return arr
-    # Escolhe um pivô aleatoriamente
+    // Escolhe um pivô aleatoriamente
     pivot_index = random.randint(0, len(arr) - 1)
     pivot = arr[pivot_index]
-    # Remove o pivô da lista
+    // Remove o pivô da lista
     arr = arr[:pivot_index] + arr[pivot_index+1:]
-    # Divide em menores e maiores
+    // Divide em menores e maiores
     menores = [x for x in arr if x <= pivot]
     maiores = [x for x in arr if x > pivot]
-    # Recursivamente ordena as partições
+    // Recursivamente ordena as partições
     return quicksort(menores) + [pivot] + quicksort(maiores)
 
 # Exemplo de uso:
